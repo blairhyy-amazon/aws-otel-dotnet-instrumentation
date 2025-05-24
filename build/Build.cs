a0cc2c2dfa93c24561e8bd2997cd8f9dd3c05604
Blair Huang <blairhyy@amazon.com>
2025-05-23 18:12:41 -0700

    fit to arm64

--- build/Build.cs
+++ build/Build.cs
@@ -84,6 +84,7 @@ internal partial class Build : NukeBuild
             case PlatformFamily.Windows:
                 fileName = "opentelemetry-dotnet-instrumentation-windows.zip";
                 break;
+            case PlatformFamily.OSX:            
             case PlatformFamily.Linux:
                 var architecture = RuntimeInformation.ProcessArchitecture;
                 string architectureSuffix;
@@ -102,9 +103,6 @@ internal partial class Build : NukeBuild
                 fileName = Environment.GetEnvironmentVariable("IsAlpine") == "true"
                     ? $"opentelemetry-dotnet-instrumentation-linux-musl-{architectureSuffix}.zip"
                     : $"opentelemetry-dotnet-instrumentation-linux-glibc-{architectureSuffix}.zip";
-                break;
-            case PlatformFamily.OSX:
-                fileName = "opentelemetry-dotnet-instrumentation-macos.zip";
                 break;
             case PlatformFamily.Unknown:
                 throw new NotSupportedException();
